990a05e18629d117118f58e069ff973519c5b618
Callum Hewitt <ch1194@york.ac.uk>
2014-11-18 13:53:46 +0000

    Initial Project Setup
    Adding all files from Initial LibGDX project set up.

---
+++ desktop/src/com/TeamHEC/LocomotionCommotion/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.TeamHEC.LocomotionCommotion.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.TeamHEC.LocomotionCommotion.LocomotionCommotion;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new LocomotionCommotion(), config);
+	}
+}
