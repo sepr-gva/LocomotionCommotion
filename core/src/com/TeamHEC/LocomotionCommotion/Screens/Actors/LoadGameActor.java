89ce6fdb26c7d410eb7fca6bebdec6f6e6ad82b9
rp825 <rp825@york.ac.uk>
2014-11-20 17:18:35 +0000

    $

--- core/src/com/TeamHEC/LocomotionCommotion/Screens/Actors/LoadGameActor.java
+++ core/src/com/TeamHEC/LocomotionCommotion/Screens/Actors/LoadGameActor.java
@@ -11,7 +11,7 @@ import com.badlogic.gdx.scenes.scene2d.InputListener;
 public class LoadGameActor extends Actor {
 
 	Texture texture = new Texture(Gdx.files.internal("sm_loadgame.png"));
-	float actorX = 450 ,actorY = 355;
+	float actorX = 450 ,actorY = 400;
 	public boolean started = false;
 
 	public LoadGameActor(){
