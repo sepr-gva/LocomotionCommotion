b6abc192ccc42d08af27a2c3d0a1e997081d752b
Callum Hewitt <ch1194@york.ac.uk>
2014-11-20 18:14:26 +0000

    First Implementation of Game.java
    Created Game.java and added CommandStack class. Will need to be
    rethought so that commands can be executed on EndTurn and benefits
    gained then. Not immediately.

---
+++ core/src/com/TeamHEC/LocomotionCommotion/Game/Game.java
@@ -0,0 +1,44 @@
+package com.TeamHEC.LocomotionCommotion.Game;
+
+import com.TeamHEC.LocomotionCommotion.Map.Map;
+import com.TeamHEC.LocomotionCommotion.Player.Player;
+
+/**
+ * 
+ * @author Callum Hewitt <ch1194@york.ac.uk>
+ *
+ */
+
+public class Game {
+	
+	//Privates
+	private Map gameMap;
+	private Player player1;
+	private Player player2;
+	private CommandStack commandStack;
+	
+	private enum PlayerTurn
+	{
+		player1,
+		player2
+	}
+		
+	/**
+	 * Initialises a Game object. This represents one instance of a game.
+	 */
+	public Game()
+	{
+		
+	}
+	
+	public void EndTurn()
+	{
+		
+	}
+	
+	public void StartTurn()
+	{
+		
+	}
+	
+}
---
+++ core/src/com/TeamHEC/LocomotionCommotion/Game/ICommand.java
@@ -0,0 +1,6 @@
+package com.TeamHEC.LocomotionCommotion.Game;
+
+public interface ICommand {
+	public void execute();
+	public void undo();
+}
