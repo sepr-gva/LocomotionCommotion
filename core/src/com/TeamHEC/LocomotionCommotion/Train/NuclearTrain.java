634a056827110d855fa922218bd7b60b13a6d425
Callum Hewitt <ch1194@york.ac.uk>
2014-11-25 20:51:37 +0000

    Revert "Commit made so I can undo."
    This reverts commit 87f07464f2607a3538aac42085d76feae2946857.

---
+++ core/src/com/TeamHEC/LocomotionCommotion/Train/NuclearTrain.java
@@ -0,0 +1,9 @@
+package com.TeamHEC.LocomotionCommotion.Train;
+
+public class NuclearTrain extends Train{
+
+	public NuclearTrain(int baseSpeed, int baseCarriageLimit, int value, boolean inStation)
+	{
+		super(baseSpeed, baseCarriageLimit, value, inStation);
+	}
+}
