87f07464f2607a3538aac42085d76feae2946857
Callum Hewitt <ch1194@york.ac.uk>
2014-11-25 20:51:34 +0000

    Commit made so I can undo.

--- core/src/com/TeamHEC/LocomotionCommotion/Train/NuclearTrain.java
+++
@@ -1,9 +0,0 @@
-package com.TeamHEC.LocomotionCommotion.Train;
-
-public class NuclearTrain extends Train{
-
-	public NuclearTrain(int baseSpeed, int baseCarriageLimit, int value, boolean inStation)
-	{
-		super(baseSpeed, baseCarriageLimit, value, inStation);
-	}
-}
