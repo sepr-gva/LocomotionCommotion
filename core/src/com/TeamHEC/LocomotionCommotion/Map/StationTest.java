e49639b13e98b4836dff12c8c2b40df359c21d08
Elliot Bray <eb1033@york.ac.uk>
2014-12-01 15:38:36 +0000

    Added station test class
    lots of methods for testing that actually need testing code in them

---
+++ core/src/com/TeamHEC/LocomotionCommotion/Map/StationTest.java
@@ -0,0 +1,129 @@
+package com.TeamHEC.LocomotionCommotion.Map;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class StationTest {
+
+	@Test
+	public void testStation() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetBaseValue() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetValueMod() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddValueMod() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSubValueMod() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetTotalValue() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetFuelType() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetBaseFuelOut() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetFuelOutMod() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddFuelOutMod() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSubFuelOutMod() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetTotalFuelOut() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetRentValue() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetRentValueMod() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddRentValueMod() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSubRentValueMod() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetTotalRent() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetOwner() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetOwner() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetFuelOutMod() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetLineType() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddListener() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testStationPurchased() {
+		fail("Not yet implemented");
+	}
+
+}
