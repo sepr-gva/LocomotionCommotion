daf2e6f1be816317e7a84fd25879486895faf6e8
rp825 <rp825@york.ac.uk>
2015-01-12 13:53:35 +0000

    nothing

--- desktop/src/com/TeamHEC/LocomotionCommotion/desktop/DesktopLauncher.java
+++ desktop/src/com/TeamHEC/LocomotionCommotion/desktop/DesktopLauncher.java
@@ -10,6 +10,7 @@ public class DesktopLauncher {
 		new LwjglApplication(LocomotionCommotion.getInstance(), config);
 		config.height= 1050;
 		config.width = 1680;
-		config.fullscreen = false;		
+		//Fullscreen
+		config.fullscreen = true;		
 	}
 }
