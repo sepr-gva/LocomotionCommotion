3f9f0bad63e989cae7220652ac12faec977b8ad1
samanderson902 <sa902@york.ac.uk>
2015-01-20 16:58:23 +0000

    tests

--- core/src/com/TeamHEC/LocomotionCommotion/Goal/Graph/Dijkstra.java
+++ core/src/com/TeamHEC/LocomotionCommotion/Goal/Graph/Dijkstra.java
@@ -20,10 +20,6 @@ public class Dijkstra {
 		map = WorldMap.getInstance();
 		stations = map.stationsList;  
 		nodeList = new Node[] {v1,v2,v3,v4,v5,v6,v7,v8,v9,v10,v11,v12,v13,v14,v15,v16,v17,v18,v19,v20,v21,v22};
-
-		
-		
-		
 		initialiseGraph();
 	}
 
