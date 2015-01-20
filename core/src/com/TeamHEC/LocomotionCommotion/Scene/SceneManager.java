fc044168cb383c9148a4ceb9ad0abc115c67f986
Callum Hewitt <callumhewitt95@gmail.com>
2015-01-20 21:23:45 +0000

    Fix problems.

--- core/src/com/TeamHEC/LocomotionCommotion/Scene/SceneManager.java
+++ core/src/com/TeamHEC/LocomotionCommotion/Scene/SceneManager.java
@@ -2,7 +2,7 @@ package com.TeamHEC.LocomotionCommotion.Scene;
 
 public class SceneManager{
 	
-	public StartScene startScene;
+	public StartMenu startScene;
 	
 	private static SceneManager instance = new SceneManager();
 	
@@ -13,7 +13,7 @@ public class SceneManager{
 	
 	private SceneManager()
 	{
-        startScene = new StartScene();
+        startScene = new StartMenu();
         startScene.addToStage();
 	}
 }
