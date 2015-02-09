ece325140870cf0b05e5522488f4eed6888b5bc0
Michael Dipper <md859@york.ac.uk>
2015-02-09 15:36:47 +0000

    Res set back to 1050p. Looking to add

--- desktop/src/com/TeamHEC/LocomotionCommotion/desktop/DesktopLauncher.java
+++ desktop/src/com/TeamHEC/LocomotionCommotion/desktop/DesktopLauncher.java
@@ -8,8 +8,8 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		new LwjglApplication(LocomotionCommotion.getInstance(), config);
-		config.width = 1280;
-		config.height = 800;
+		config.width = 1680;
+		config.height = 1050;
 		config.fullscreen = false;	
 	}
 }
